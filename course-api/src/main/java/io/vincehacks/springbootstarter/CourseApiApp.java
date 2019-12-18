56632305a5f154076b987e161d50c7965cabc725
Vince Chang <vincechangsf@gmail.com>
2019-12-17 22:15:12 -0800

    Adding a spring boot starter project

---
+++ course-api/src/main/java/io/vincehacks/springbootstarter/CourseApiApp.java
@@ -0,0 +1,15 @@
+package io.vincehacks.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApp {
+
+	// This is the entry point of the application, this will automatically have a servlet container for my app
+	// Sets up default configuration, starts Spring application context, performs class path scan, starts Tomcat server
+	public static void main(String[] args) {
+		// Tell Spring this is a spring application
+		SpringApplication.run(CourseApiApp.class,args);
+	}
+}
---
+++ course-api/src/main/java/io/vincehacks/springbootstarter/hello/HelloController.java
@@ -0,0 +1,15 @@
+package io.vincehacks.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	// By default this handles GET requests
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "Hi";
+	}
+
+}
