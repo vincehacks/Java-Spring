edc7eaf5cfb321b7f32056e01d3c61584ed043c8
Vince Chang <vincechangsf@gmail.com>
2019-12-18 13:43:23 -0800

    Adding more controllers and functionality

--- course-api/src/main/java/io/vincehacks/springbootstarter/topic/TopicController.java
+++ course-api/src/main/java/io/vincehacks/springbootstarter/topic/TopicController.java
@@ -1,23 +1,37 @@
 package io.vincehacks.springbootstarter.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class TopicController {
 
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		
-		return Arrays.asList(
-			new Topic("spring","Spring Framework","Spring Framework Description"),
-			new Topic("java","Core Java","Core Java Description"),
-			new Topic("javascript","JavaScript","JavaScript Description")			
-				
-		);
+		return topicService.getAllTopics();		
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
 	}
 }
+
+
+
+
---
+++ course-api/src/main/java/io/vincehacks/springbootstarter/topic/TopicService.java
@@ -0,0 +1,37 @@
+package io.vincehacks.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	List<Topic> list = new ArrayList<>(Arrays.asList(
+			new Topic("spring","Spring Framework","Spring Framework Description"),
+			new Topic("java","Core Java","Core Java Description"),
+			new Topic("javascript","JavaScript","JavaScript Description")			
+				
+		));
+	
+	List<Topic> getAllTopics(){
+		return list;
+	}
+	
+	Topic getTopic(String id) {
+		//return list.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		for(Topic t : list) {
+			if(t.getId().equals(id)) {
+				return t;
+			}
+		}
+		
+		return new Topic();
+	}
+	
+	void addTopic(Topic topic) {
+		list.add(topic);
+	}
+}
