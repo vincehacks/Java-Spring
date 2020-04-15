696986fcf9786d7b6fd768cc7a46c9aed4af3c81
Vince Chang <vincechangsf@gmail.com>
2020-04-15 12:47:45 -0700

    tabs to spaces

--- aliens/src/main/java/com/aliens/aliens/AlienController.java
+++ aliens/src/main/java/com/aliens/aliens/AlienController.java
@@ -17,45 +17,45 @@ public class AlienController {
 
   AlienDAO repo = new AlienDAO();
 
-	// Retrieves ALL ALIENS from the DB
-	@GetMapping("aliens")
-	public List<Alien> getAliens(){
-		return repo.getAliens();
-	}
+  // Retrieves ALL ALIENS from the DB
+  @GetMapping("aliens")
+  public List<Alien> getAliens(){
+    return repo.getAliens();
+  }
 
-	// Retrieves ONE alien from the DB
-	@GetMapping("alien/{id}")
-	public Alien getAlien(@PathVariable("id") int id){
-		return repo.getAlien(id);
-	}
+  // Retrieves ONE alien from the DB
+  @GetMapping("alien/{id}")
+  public Alien getAlien(@PathVariable("id") int id){
+    return repo.getAlien(id);
+  }
 
-	// Adds an alien to the DB
-	@PostMapping("alien")
-	public Alien createAlien(@RequestBody Alien a1){
-		repo.createAlien(a1);
-		return a1;
-	}
+  // Adds an alien to the DB
+  @PostMapping("alien")
+  public Alien createAlien(@RequestBody Alien a1){
+    repo.createAlien(a1);
+    return a1;
+  }
 
-	// Updates an alien in the DB, creates a new alien of id does not exist!
-	@PutMapping("alien")
-	public Alien updateAlien(@RequestBody Alien a1){
-		if(repo.getAlien(a1.getId()).getId() == 0)
-			repo.createAlien(a1);
-		else
-			repo.updateAlien(a1);
+  // Updates an alien in the DB, creates a new alien of id does not exist!
+  @PutMapping("alien")
+  public Alien updateAlien(@RequestBody Alien a1){
+    if(repo.getAlien(a1.getId()).getId() == 0)
+      repo.createAlien(a1);
+    else
+      repo.updateAlien(a1);
 
-		return a1;
-	}
+    return a1;
+  }
 
-	// Deletes an alien from the DB, if it's in the DB
-	@DeleteMapping("alien/{id}")
-	public Alien deleteAlien(@PathVariable("id") int id){
+  // Deletes an alien from the DB, if it's in the DB
+  @DeleteMapping("alien/{id}")
+  public Alien deleteAlien(@PathVariable("id") int id){
 
-		Alien a = repo.getAlien(id);
+    Alien a = repo.getAlien(id);
 
-		if(a.getId() != 0)
-			repo.deleteAlien(id);
+    if(a.getId() != 0)
+      repo.deleteAlien(id);
 
-		return a;
-	}
+    return a;
+  }
 }
