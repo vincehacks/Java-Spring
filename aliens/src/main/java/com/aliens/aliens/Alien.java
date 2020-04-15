d7a41c0c2840c1d0fd8e7b3c9422cb0c1bb0c9c6
Vince Chang <vincechangsf@gmail.com>
2020-04-15 12:46:52 -0700

    tabs to spaces

--- aliens/src/main/java/com/aliens/aliens/Alien.java
+++ aliens/src/main/java/com/aliens/aliens/Alien.java
@@ -3,31 +3,31 @@
 package com.aliens.aliens;
 
 public class Alien {
-	private int id;
-	private String name;
-	private int points;
+  private int id;
+  private String name;
+  private int points;
 
-	public void setId(int id){
-		this.id = id;
-	}
+  public void setId(int id){
+    this.id = id;
+  }
 
-	public int getId(){
-		return id;
-	}
+  public int getId(){
+    return id;
+  }
 
-	public void setName(String name){
-		this.name = name;
-	}
+  public void setName(String name){
+    this.name = name;
+  }
 
-	public String getName(){
-		return name;
-	}
+  public String getName(){
+    return name;
+  }
 
-	public void setPoints(int points){
-		this.points = points;
-	}
+  public void setPoints(int points){
+    this.points = points;
+  }
 
-	public int getPoints(){
-		return points;
-	}
+  public int getPoints(){
+    return points;
+  }
 }
--- aliens/src/main/java/com/aliens/aliens/AlienDAO.java
+++ aliens/src/main/java/com/aliens/aliens/AlienDAO.java
@@ -8,172 +8,172 @@ import java.sql.*;
 
 public class AlienDAO {
 
-	// Creating a Connection object to connect to MySQL database
-	Connection con = null;
+  // Creating a Connection object to connect to MySQL database
+  Connection con = null;
 
-	/* =========================================================================
-	* Function Name: constructor
-	* Task: Uses JDBC is the API to connect to MySQL database
-		========================================================================= */
-	public AlienDAO(){
+  /* =========================================================================
+  * Function Name: constructor
+  * Task: Uses JDBC is the API to connect to MySQL database
+    ========================================================================= */
+  public AlienDAO(){
 
-		// THIS DID NOT WORK (I THINK BECAUSE DIFFERENT VERSION)
-		// String url = "jdbc:mysql://localhost:3306/restdb?autoReconnect=true&useSSL=false";
+    // THIS DID NOT WORK (I THINK BECAUSE DIFFERENT VERSION)
+    // String url = "jdbc:mysql://localhost:3306/restdb?autoReconnect=true&useSSL=false";
 
-		// THIS WORKS!
-		String url = "jdbc:mysql://localhost:3306/restdb?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=GMT";
+    // THIS WORKS!
+    String url = "jdbc:mysql://localhost:3306/restdb?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=GMT";
 
-		String username = "root";
-		String password = "adminadmin";
+    String username = "root";
+    String password = "adminadmin";
 
-		// Will need to be in a try catch block
-		try{
-			// Loading a driver and getting a connection object to MySQL DB
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection(url,username,password);
-		}
-		catch(Exception e){
-			System.out.println(e);
-		}
-	}
+    // Will need to be in a try catch block
+    try{
+      // Loading a driver and getting a connection object to MySQL DB
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      con = DriverManager.getConnection(url,username,password);
+    }
+    catch(Exception e){
+      System.out.println(e);
+    }
+  }
 
-	/* =========================================================================
-	* Function Name: getAliens
-	* Task: Returns back a list of Aliens by using JDBC.
-	* STEPS:
-	*		1. Create a SQL statement as a string
-	* 	2. In a try catch block, use Connection object to create a Statement
-	* 	object and then use the Statement object to execute the SQL query from
-	* 	step 1
-	*   3. Step 2 will return a Result set, so will need to use a while loop to
-	*   iterate over everything in the set. In this case, for each row, will
-	*   create a new Alien object and add this to the list
-		========================================================================= */
-	public List<Alien> getAliens(){
+  /* =========================================================================
+  * Function Name: getAliens
+  * Task: Returns back a list of Aliens by using JDBC.
+  * STEPS:
+  *		1. Create a SQL statement as a string
+  * 	2. In a try catch block, use Connection object to create a Statement
+  * 	object and then use the Statement object to execute the SQL query from
+  * 	step 1
+  *   3. Step 2 will return a Result set, so will need to use a while loop to
+  *   iterate over everything in the set. In this case, for each row, will
+  *   create a new Alien object and add this to the list
+    ========================================================================= */
+  public List<Alien> getAliens(){
 
-		List<Alien> aliens = new ArrayList<>();
-		String sql = "SELECT * FROM alien";
+    List<Alien> aliens = new ArrayList<>();
+    String sql = "SELECT * FROM alien";
 
-		try{
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-			while(rs.next()){
-				Alien a = new Alien();
-				a.setId(rs.getInt(1));
-				a.setName(rs.getString(2));
-				a.setPoints(rs.getInt(3));
+    try{
+      Statement st = con.createStatement();
+      ResultSet rs = st.executeQuery(sql);
+      while(rs.next()){
+        Alien a = new Alien();
+        a.setId(rs.getInt(1));
+        a.setName(rs.getString(2));
+        a.setPoints(rs.getInt(3));
 
-				aliens.add(a);
-			}
-		}
-		catch(Exception e){
-			System.out.println(e);
-		}
+        aliens.add(a);
+      }
+    }
+    catch(Exception e){
+      System.out.println(e);
+    }
 
-		return aliens;
-	}
+    return aliens;
+  }
 
-	/* =========================================================================
-	* Function Name: getAlien
-	* Task: Returns back a one alien that matches the id that is passed in
-	* STEPS:
-	*		1. Create a SQL statement as a string
-	* 	2. In a try catch block, use Connection object to create a Statement
-	* 	object and then use the Statement object to execute the SQL query from
-	* 	step 1
-	*   3. Step 2 will return a Result set, DO NOT NEED A WHILE LOOP because will
-	*   only need to get one value back from the DB
-		========================================================================= */
-	public Alien getAlien(int id){
+  /* =========================================================================
+  * Function Name: getAlien
+  * Task: Returns back a one alien that matches the id that is passed in
+  * STEPS:
+  *		1. Create a SQL statement as a string
+  * 	2. In a try catch block, use Connection object to create a Statement
+  * 	object and then use the Statement object to execute the SQL query from
+  * 	step 1
+  *   3. Step 2 will return a Result set, DO NOT NEED A WHILE LOOP because will
+  *   only need to get one value back from the DB
+    ========================================================================= */
+  public Alien getAlien(int id){
 
-		String sql = "SELECT * FROM alien WHERE id=" + id;
-		Alien a = new Alien();
+    String sql = "SELECT * FROM alien WHERE id=" + id;
+    Alien a = new Alien();
 
-		try{
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(sql);
-			if(rs.next()){
-				a.setId(rs.getInt(1));
-				a.setName(rs.getString(2));
-				a.setPoints(rs.getInt(3));
-			}
-		}
-		catch(Exception e){
-			System.out.println(e);
-		}
+    try{
+      Statement st = con.createStatement();
+      ResultSet rs = st.executeQuery(sql);
+      if(rs.next()){
+        a.setId(rs.getInt(1));
+        a.setName(rs.getString(2));
+        a.setPoints(rs.getInt(3));
+      }
+    }
+    catch(Exception e){
+      System.out.println(e);
+    }
 
-		return a;
-	}
+    return a;
+  }
 
-	/* =========================================================================
-	* Function Name: createAlien
-	* Task: Adds a alien to the DB using a PreparedStatement object
-	* Using a PreparedStatement because it will replace the question marks by
-	* using teh Alien object I passed in to fill those values
-	========================================================================= */
-	public void createAlien(Alien a){
+  /* =========================================================================
+  * Function Name: createAlien
+  * Task: Adds a alien to the DB using a PreparedStatement object
+  * Using a PreparedStatement because it will replace the question marks by
+  * using teh Alien object I passed in to fill those values
+  ========================================================================= */
+  public void createAlien(Alien a){
 
-		// Instead of doing this below, utilize a PreparedStatement from java.sql
-		// You can use ? as a placeholder !!!
-		// String sql = "INSERT INTO alien VALUES ("  + a.getId() + ", " + a.getName()
-		// + ", " a.getPoints() + ")";
+    // Instead of doing this below, utilize a PreparedStatement from java.sql
+    // You can use ? as a placeholder !!!
+    // String sql = "INSERT INTO alien VALUES ("  + a.getId() + ", " + a.getName()
+    // + ", " a.getPoints() + ")";
 
-		String sql = "INSERT INTO alien VALUES (?,?,?)";
+    String sql = "INSERT INTO alien VALUES (?,?,?)";
 
-		try{
-			// The PreparedStatement takes the sql query right away vs a Statement obj
-			PreparedStatement st = con.prepareStatement(sql);
-			// Replacing the ? now in the sql string
-			st.setInt(1, a.getId());
-			st.setString(2,a.getName());
-			st.setInt(3,a.getPoints());
-			st.executeUpdate();
-		}
-		catch(Exception e){
-			System.out.println(e);
-		}
-	}
+    try{
+      // The PreparedStatement takes the sql query right away vs a Statement obj
+      PreparedStatement st = con.prepareStatement(sql);
+      // Replacing the ? now in the sql string
+      st.setInt(1, a.getId());
+      st.setString(2,a.getName());
+      st.setInt(3,a.getPoints());
+      st.executeUpdate();
+    }
+    catch(Exception e){
+      System.out.println(e);
+    }
+  }
 
 
-	/* =========================================================================
-	* Function Name: updateAlien
-	* Task: Updates an Alien's information in the DB
-	========================================================================= */
-	public void updateAlien(Alien a){
+  /* =========================================================================
+  * Function Name: updateAlien
+  * Task: Updates an Alien's information in the DB
+  ========================================================================= */
+  public void updateAlien(Alien a){
 
-		String sql = "UPDATE alien SET name=?, points=? WHERE id=?";
+    String sql = "UPDATE alien SET name=?, points=? WHERE id=?";
 
-		try{
-			// The PreparedStatement takes the sql query right away vs a Statement obj
-			PreparedStatement st = con.prepareStatement(sql);
-			// Replacing the ? now in the sql string
-			st.setString(1,a.getName());
-			st.setInt(2,a.getPoints());
-			st.setInt(3,a.getId());
-			st.executeUpdate();
-		}
-		catch(Exception e){
-			System.out.println(e);
-		}
-	}
+    try{
+      // The PreparedStatement takes the sql query right away vs a Statement obj
+      PreparedStatement st = con.prepareStatement(sql);
+      // Replacing the ? now in the sql string
+      st.setString(1,a.getName());
+      st.setInt(2,a.getPoints());
+      st.setInt(3,a.getId());
+      st.executeUpdate();
+    }
+    catch(Exception e){
+      System.out.println(e);
+    }
+  }
 
-	/* =========================================================================
-	* Function Name: deleteAlien
-	* Task: Deletes the alien from the DB
-	========================================================================= */
-	public void deleteAlien(int id){
+  /* =========================================================================
+  * Function Name: deleteAlien
+  * Task: Deletes the alien from the DB
+  ========================================================================= */
+  public void deleteAlien(int id){
 
-		String sql = "DELETE FROM alien WHERE id=?";
+    String sql = "DELETE FROM alien WHERE id=?";
 
-		try{
-			// The PreparedStatement takes the sql query right away vs a Statement obj
-			PreparedStatement st = con.prepareStatement(sql);
-			// Replacing the ? now in the sql string
-			st.setInt(1,id);
-			st.executeUpdate();
-		}
-		catch(Exception e){
-			System.out.println(e);
-		}
-	}
+    try{
+      // The PreparedStatement takes the sql query right away vs a Statement obj
+      PreparedStatement st = con.prepareStatement(sql);
+      // Replacing the ? now in the sql string
+      st.setInt(1,id);
+      st.executeUpdate();
+    }
+    catch(Exception e){
+      System.out.println(e);
+    }
+  }
 }
